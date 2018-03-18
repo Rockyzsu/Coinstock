dd0073a264e90758040580d6b94ab14bce9919eb
Rockyzsu <jinweizsu@gmail.com>
2018-03-18 15:47:32 +0800

    update

--- coinegg/BalanceService.py
+++ coinegg/BalanceService.py
@@ -7,6 +7,8 @@ import BaseService
 import Utils
 from urllib import urlencode
 import pandas as pd
+
+
 class BalanceService(BaseService.BaseService):
 	POST_BALANCE = 'balance'
 	POST_TRADE_LIST = 'trade_list'
@@ -17,7 +19,7 @@ class BalanceService(BaseService.BaseService):
 		try:
 			f = open('keys.json', 'r')
 			conf = json.load(f)
-			self.public_key = conf['query']['public_key'] 
+			self.public_key = conf['query']['public_key']
 			self.private_key = conf['query']['private_key']
 		except Exception as e:
 			raise
@@ -51,34 +53,33 @@ class BalanceService(BaseService.BaseService):
 		btc_lock - 比特币冻结余额
 		'''
 		nonce = Utils.generate_nonce()
-		original_params = {'nonce' : nonce, 'key' : self.public_key}
+		original_params = {'nonce': nonce, 'key': self.public_key}
 		params = Utils.reformat_params(original_params, self.private_key)
 		url = self.restful_url + BalanceService.POST_BALANCE
 		self.logger.info('post_balance: %s, %s' % (url, params))
-		response = requests.post(url, data = params)
-		return response.json() if response else ''			
+		response = requests.post(url, data=params)
+		return response.json() if response else ''
 
-	def post_trade_list(self, coin, since = 0, trade_type = 'all'):
+	def post_trade_list(self, coin, since=0, trade_type='all'):
 		'''
 		您指定时间后的挂单，可以根据类型查询，比如查看正在挂单和全部挂单
 		'''
 		nonce = Utils.generate_nonce()
-		original_params = {'coin' : coin, 'since' : since, 'type' : trade_type, 'nonce' : nonce, 'key' : self.public_key}
+		original_params = {'coin': coin, 'since': since, 'type': trade_type, 'nonce': nonce, 'key': self.public_key}
 		params = Utils.reformat_params(original_params, self.private_key)
 		url = self.restful_url + BalanceService.POST_TRADE_LIST
 		self.logger.info('post_trade_list: %s, %s' % (url, params))
-		response = requests.post(url, data = params)
-		return response.json() if response else ''	
+		response = requests.post(url, data=params)
+		return response.json() if response else ''
 
 	def post_trade_view(self, coin, trade_id):
 		nonce = Utils.generate_nonce()
-		original_params = {'coin' : coin, 'id' : trade_id, 'nonce' : nonce, 'key' : self.public_key}
+		original_params = {'coin': coin, 'id': trade_id, 'nonce': nonce, 'key': self.public_key}
 		params = Utils.reformat_params(original_params, self.private_key)
 		url = self.restful_url + BalanceService.POST_TRADE_VIEW
 		self.logger.info('post_trade_view: %s, %s' % (url, params))
-		response = requests.post(url, data = params)
-		return response.json() if response else ''		
-
+		response = requests.post(url, data=params)
+		return response.json() if response else ''
 
 
 if __name__ == '__main__':
--- coinegg/worker.py
+++ coinegg/worker.py
@@ -1,16 +1,18 @@
-#*-*coding:utf-8-*-
+# *-*coding:utf-8-*-
 import BalanceService
 import pandas as pd
-import Utils,re
-import itchat,time
+import Utils, re
+import itchat, time
 from itchat.content import *
-DB_NAME='db_coin'
+
+DB_NAME = 'db_coin'
+
 
 def store_data():
-	coin_name='zet'
+	coin_name = 'zet'
 	balanceService = BalanceService.BalanceService()
-	db=Utils.mongo("coin")
-	collection=db[coin_name]
+	db = Utils.mongo("coin")
+	collection = db[coin_name]
 	# balance = balanceService.post_balance()
 	# print balance
 	trade_list = balanceService.post_trade_list(coin_name, 0, 'all')
@@ -32,40 +34,43 @@ def store_data():
 
 	for trade in trade_list.get('data'):
 		collection.insert(trade)
-	# 	print trade
-
-		# if trade.get('status')=='closed':
-		# print "datetime: {} ".format(trade.get('datetime')),
-		# print "type: {} ".format(trade.get('type')),
-		# print "status: {} ".format(trade.get('status')),
-		# print "price: {} ".format(trade.get('price')),
-		# print "amount_original: {} ".format(trade.get('amount_original'))
 
 
-	# trade_view = balanceService.post_trade_view('doge', 6860502)
-	# print trade_view
+# 	print trade
 
-	# print df
+# if trade.get('status')=='closed':
+# print "datetime: {} ".format(trade.get('datetime')),
+# print "type: {} ".format(trade.get('type')),
+# print "status: {} ".format(trade.get('status')),
+# print "price: {} ".format(trade.get('price')),
+# print "amount_original: {} ".format(trade.get('amount_original'))
+
+
+# trade_view = balanceService.post_trade_view('doge', 6860502)
+# print trade_view
+
+# print df
 def analysis():
-	db=Utils.mongo("coin")
-	coin_name='zet'
+	db = Utils.mongo("coin")
+	coin_name = 'zet'
 
-	collection=db[coin_name]
-	total=[]
-	for item in collection.find({'status':'closed'}):
+	collection = db[coin_name]
+	total = []
+	for item in collection.find({'status': 'closed'}):
 		# print type(item)
-		#print item
+		# print item
 		price = float(item.get('price'))
-		if item.get('type')==u'sell':
-			price=-1.0*price
-		amount_original=float(item.get('amount_original'))
-		total.append(amount_original*price)
+		if item.get('type') == u'sell':
+			price = -1.0 * price
+		amount_original = float(item.get('amount_original'))
+		total.append(amount_original * price)
 	print total
 	print sum(total)
 
+
 # @itchat.msg_register([TEXT])
 def wechat_monitor(content):
-	user=Utils.js.get('wechat_user')
+	user = Utils.js.get('wechat_user')
 	# print user
 	account = itchat.get_friends(u'wei')
 	# print 'acount {}'.format(account)
@@ -74,9 +79,9 @@ def wechat_monitor(content):
 		# print type(i)
 		# print i
 		if i[u'PYQuanPin'] == u'wei':
-			username= i['UserName']
-			# print username
-		# print i
+			username = i['UserName']
+		# print username
+	# print i
 	# itchat.send(content, toUserName=id)
 
 	# print 'done'
@@ -86,48 +91,52 @@ def wechat_monitor(content):
 '''
 监测市场的行情
 '''
+
+
 def detect_market(focus_list=[]):
 	ret_coin_info = Utils.coinegg_coins()
 	if not ret_coin_info:
 		print 'fail'
 		return None
-	resullt=dict()
-	red=0
-	counts=0
-	for coin,detail in ret_coin_info.items():
+	resullt = dict()
+	red = 0
+	counts = 0
+	for coin, detail in ret_coin_info.items():
 		# print coin,'\t',detail[8]
-		if detail[8]>0:
-			red+=1
-		counts+=1
-		resullt[coin]=detail[8]
+		if detail[8] > 0:
+			red += 1
+		counts += 1
+		resullt[coin] = detail[8]
 
-		if detail[8]>20:
-			wechat_monitor(u'coin {} is rasing to {}, go to focus!'.format(coin,detail[8]))
+		if detail[8] > 20:
+			wechat_monitor(u'coin {} is rasing to {}, go to focus!'.format(coin, detail[8]))
 	# print max(resullt.values())
 	# lens=len(resullt)
 	# wechat_monitor(red/(1.0*counts)*100)
-	if red/(1.0*counts)*100>=30:
+	if red / (1.0 * counts) * 100 >= 30:
 		# print "Hot !!!"
-		wechat_monitor(u'ICO is hotting, more than {} coin percentage is red go to focus!'.format(red/(1.0*counts)*100))
+		wechat_monitor(
+			u'ICO is hotting, more than {} coin percentage is red go to focus!'.format(red / (1.0 * counts) * 100))
 
-	if  focus_list:
+	if focus_list:
 		for coin in focus_list:
-			if resullt[coin] >10:
-				wechat_monitor(u'Coin {} is rasing as {}'.format(coin,resullt[coin]))
+			if resullt[coin] > 10:
+				wechat_monitor(u'Coin {} is rasing as {}'.format(coin, resullt[coin]))
 
 
 def main():
-	MINUTES=60
-	coin_list=['zet','ifc','mryc']
+	MINUTES = 60
+	coin_list = ['zet', 'ifc', 'mryc']
 	while 1:
 		# store_data()
 		# analysis()
 
 		detect_market(coin_list)
-		time.sleep(30*MINUTES)
-		# itchat.auto_login()
+		time.sleep(30 * MINUTES)
+	# itchat.auto_login()
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
 	itchat.auto_login(hotReload=True)
 	# itchat.run()
 	print 'go to main'
